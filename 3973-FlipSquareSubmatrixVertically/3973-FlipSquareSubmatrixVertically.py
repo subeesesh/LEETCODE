0ca0602f3d5c1ff95e42adfe3b9779d29e4bf01b
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:51:21 +0530

    Create 3973-FlipSquareSubmatrixVertically

---
+++ 3973-FlipSquareSubmatrixVertically/3973-FlipSquareSubmatrixVertically.py
@@ -0,0 +1,9 @@
+# Last updated: 5/29/2026, 11:51:29 AM
+class Solution:
+    def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
+        t, b=x, x+k-1
+        while t<b:
+            grid[t][y:y+k], grid[b][y:y+k]=grid[b][y:y+k],grid[t][y:y+k]
+            t+=1
+            b-=1
+        return grid
