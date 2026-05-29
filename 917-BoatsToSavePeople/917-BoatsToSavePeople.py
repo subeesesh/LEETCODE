379f1e6b473c26281a7a8bc735c470bbed1d2b7a
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:54:02 +0530

    Create 917-BoatsToSavePeople

---
+++ 917-BoatsToSavePeople/917-BoatsToSavePeople.py
@@ -0,0 +1,13 @@
+# Last updated: 5/29/2026, 11:54:09 AM
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        i=0
+        j=len(people)-1
+        count=0
+        while(i<=j):
+            if people[i]+people[j]<=limit:
+                i+=1
+            j-=1
+            count+=1
+        return count
