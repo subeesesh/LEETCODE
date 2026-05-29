d8f63b1e32312e4989e01dbac302fe74fc74e026
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:53:49 +0530

    Create 988-FlipEquivalentBinaryTrees

---
+++ 988-FlipEquivalentBinaryTrees/988-FlipEquivalentBinaryTrees.py
@@ -0,0 +1,20 @@
+# Last updated: 5/29/2026, 11:53:57 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        if not root1 and not root2:
+            return True
+        if not root1 and root2:
+            return False
+        if root1 and not root2:
+            return False
+        if root1.val!=root2.val:
+            return False
+        notf=self.flipEquiv(root1.left,root2.left) and self.flipEquiv(root1.right,root2.right)
+        f=self.flipEquiv(root1.left,root2.right) and self.flipEquiv(root1.right,root2.left)
+        return notf or f
