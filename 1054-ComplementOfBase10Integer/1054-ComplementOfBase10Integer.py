4aa267a32d6cd0370d1b2b90e99a01ecdbc8c77d
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:53:37 +0530

    Create 1054-ComplementOfBase10Integer

---
+++ 1054-ComplementOfBase10Integer/1054-ComplementOfBase10Integer.py
@@ -0,0 +1,8 @@
+# Last updated: 5/29/2026, 11:53:44 AM
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        if n == 0:
+            return 1
+        bit_length = n.bit_length()
+        mask = (1 << bit_length) - 1
+        return n ^ mask
