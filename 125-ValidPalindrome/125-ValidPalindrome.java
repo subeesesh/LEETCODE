43c0d67a341f3024c1d5123808a45cc8f75e63ab
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:58:35 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,21 @@
+// Last updated: 5/29/2026, 11:58:42 AM
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+        }
+        String str=sb.toString();
+        int i=0,j=str.length()-1;
+        while(i<j){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
