661266fbc2679f9bb85491d7e0ce64219207960b
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:59:35 +0530

    Create 72-EditDistance

---
+++ 72-EditDistance/72-EditDistance.py
@@ -0,0 +1,23 @@
+# Last updated: 5/29/2026, 11:59:42 AM
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n=len(word1)
+        m=len(word2)
+        if word1=="" or word2=="":
+            return max(n,m)
+        if word1==word2:
+            return 0
+        dp=[[0 for _ in range(m+1)]for _ in range(n+1)]
+        for i in range(n+1):
+            dp[i][0]=i
+        for j in range(m+1):
+            dp[0][j]=j
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j]=1+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+        return dp[n][m]
+
+        
