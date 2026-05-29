e1ca37469acfb2e9754e44216347b2119dbf1544
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:51:34 +0530

    Create 3496-MinimumNumberOfSecondsToMakeMountainHeightZero

---
+++ 3496-MinimumNumberOfSecondsToMakeMountainHeightZero/3496-MinimumNumberOfSecondsToMakeMountainHeightZero.py
@@ -0,0 +1,17 @@
+# Last updated: 5/29/2026, 11:51:41 AM
+class Solution:
+    def minNumberOfSeconds(self, height: int, times: list[int]) -> int:
+        lo, hi = 1, 10**16
+
+        while lo < hi:
+            mid = (lo + hi) >> 1
+            tot = 0
+            for t in times:
+                tot += int(math.sqrt(mid / t * 2 + 0.25) - 0.5)
+                if tot >= height: break
+            if tot >= height:
+                hi = mid
+            else:
+                lo = mid + 1
+
+        return lo
