d874fbabf3b12f40c0e4743bca24da46be791415
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:59:08 +0530

    Create 90-SubsetsIi

---
+++ 90-SubsetsIi/90-SubsetsIi.py
@@ -0,0 +1,18 @@
+# Last updated: 5/29/2026, 11:59:15 AM
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+
+
+        def backtrack(start, path):
+            result.append(path[:])  
+            for i in range(start, len(nums)):
+                if  i > start and nums[i] == nums[i - 1]:
+                    continue
+                path.append(nums[i])       
+                backtrack(i + 1, path)     
+                path.pop()                 
+
+        backtrack(0, [])
+        return result
