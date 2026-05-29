2a6e88012628232f5d69123ff4a073e294602d4d
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:57:07 +0530

    Create 208-ImplementTriePrefixTree

---
+++ 208-ImplementTriePrefixTree/208-ImplementTriePrefixTree.py
@@ -0,0 +1,36 @@
+# Last updated: 5/29/2026, 11:57:14 AM
+class Trie:
+
+    def __init__(self):
+        self.root={}
+        
+    def insert(self, word: str) -> None:
+
+        cur=self.root
+
+        for letter in word:
+            if letter not in cur:
+                cur[letter]={}
+            cur=cur[letter]
+
+        cur['*']=''
+
+    def search(self, word: str) -> bool:
+
+        cur=self.root
+        for letter in word:
+            if letter not in cur:
+                return False
+            cur=cur[letter]
+
+        return '*' in cur
+        
+    def startsWith(self, prefix: str) -> bool:
+
+        cur=self.root
+        for letter in prefix:
+            if letter not in cur:
+                return False
+            cur=cur[letter]
+
+        return True
