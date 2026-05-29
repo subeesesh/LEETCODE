c3631727269e82fa3205ab3831273d2991501e0f
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:53:42 +0530

    Create 1019-SquaresOfASortedArray

---
+++ 1019-SquaresOfASortedArray/1019-SquaresOfASortedArray.py
@@ -0,0 +1,13 @@
+# Last updated: 5/29/2026, 11:53:49 AM
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res=[]
+        l,r=0,len(nums)-1
+        while l<=r:
+            if nums[l]*nums[l]>nums[r]*nums[r]:
+                res.append(nums[l]*nums[l])
+                l+=1
+            else:
+                res.append(nums[r]*nums[r])
+                r-=1
+        return res[::-1]
