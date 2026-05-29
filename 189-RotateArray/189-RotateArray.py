f5cc901864a39785acfbadf7a289bdaecde34a27
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:57:28 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.py
@@ -0,0 +1,15 @@
+# Last updated: 5/29/2026, 11:57:36 AM
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
+
+    def reverse(self, nums: list[int], left: int, right: int) -> None:
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
