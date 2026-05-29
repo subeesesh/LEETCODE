52336740f4539b359b4a41b12717a30f5934952b
V K Shri Subeesesh <162684671+subeesesh@users.noreply.github.com>
2026-05-29 11:52:55 +0530

    Create 1704-SpecialPositionsInABinaryMatrix

---
+++ 1704-SpecialPositionsInABinaryMatrix/1704-SpecialPositionsInABinaryMatrix.py
@@ -0,0 +1,18 @@
+# Last updated: 5/29/2026, 11:53:02 AM
+class Solution:
+    def numSpecial(self, mat):
+        m, n = len(mat), len(mat[0])
+        row = [0] * m
+        col = [0] * n
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1:
+                    row[i] += 1
+                    col[j] += 1
+
+        return sum(
+            mat[i][j] == 1 and row[i] == 1 and col[j] == 1
+            for i in range(m)
+            for j in range(n)
+        )
